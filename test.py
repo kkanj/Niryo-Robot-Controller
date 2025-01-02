e7ba1f5d3127f0f1c49fb8c566c57e7754767df0
Krishan Kanji <krishankanji2003@gmail.com>
2025-01-01 21:14:21 -0800

    added test.py to test pusblishing movements to the  robot

---
+++ test.py
@@ -0,0 +1,41 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+def move_robot():
+    rospy.init_node('test_movement')
+    pub = rospy.Publisher('/niryo_one/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(1)  # 1 Hz
+
+    # Define a simple movement pattern
+    movements = [
+        (0.1, 0, 0, 0),  # Move forward
+        (-0.1, 0, 0, 0), # Move backward
+        (0, 0.1, 0, 0),  # Move left
+        (0, -0.1, 0, 0), # Move right
+        (0, 0, 0.1, 0),  # Move up
+        (0, 0, -0.1, 0), # Move down
+        (0, 0, 0, 0.1),  # Rotate clockwise
+        (0, 0, 0, -0.1)  # Rotate counterclockwise
+    ]
+
+    try:
+        for movement in movements:
+            if rospy.is_shutdown():
+                break
+            twist = Twist()
+            twist.linear.x = movement[0]
+            twist.linear.y = movement[1]
+            twist.linear.z = movement[2]
+            twist.angular.z = movement[3]
+            pub.publish(twist)
+            print("Published movement: {}".format(movement))
+            rate.sleep()
+
+        # Stop the robot after the movements
+        pub.publish(Twist())
+        print("Test completed. Robot stopped.")
+    except rospy.ROSInterruptException:
+        pass
+
+if __name__ == '__main__':
+    move_robot()
